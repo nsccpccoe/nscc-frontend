e93a4e9a4deeb2689e39052b0f2c483d87cbb414
AtharvaKhule-12 <khuleatharva@gmail.com>
2023-01-14 00:22:27 +0530

    created a 'test' page to display contest components

---
+++ pages/test.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Contest_desc from '../Components/Contest_desc/Contest_desc'
+import Contest_prizes from '../Components/Contest_prizes/Contest_prizes'
+
+const test = () => {
+  return (
+    <div>
+        <Contest_desc/>
+        <Contest_prizes/>
+    </div>
+  )
+}
+
+export default test
