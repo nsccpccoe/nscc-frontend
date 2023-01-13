6f8d82c8d51183b8d091150e7081b45ae0a26692
gayatri228 <gayatri7588sharmaa@gmail.com>
2023-01-13 22:01:46 +0530

    Added timeline2

--- Components/Timeline/Timeline.tsx
+++ Components/Timeline/Timeline.tsx
@@ -22,7 +22,8 @@ export default function TimelineF() {
             color="grey"
             className="styles.timing"
           >
-            9:30 am
+           <Typography>Thu Jan 22 2023 IST</Typography>
+          <Typography>08:00:00 AM</Typography>
           </TimelineOppositeContent>
           <TimelineSeparator>
             <TimelineConnector
@@ -46,16 +47,14 @@ export default function TimelineF() {
               color={"white"}
               className={styles.shape}
             >
-              Freelancer
+             NSCC WebXplore
             </Typography>
             <Typography
               color={"white"}
               fontStyle={"italic"}
               className={styles.text}
             >
-              {" "}
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam,
-              optio .{" "}
+              Hackathon Begins
             </Typography>
           </TimelineContent>
         </TimelineItem>
@@ -66,7 +65,8 @@ export default function TimelineF() {
             color={"grey"}
             className="styles.timing"
           >
-            10:00 am
+           <Typography>Thu Jan 26 2023 IST</Typography>
+          <Typography>02:00:00 PM - 05:00:00 PM (3hrs)</Typography>
           </TimelineOppositeContent>
           <TimelineSeparator>
             <TimelineConnector
@@ -86,16 +86,14 @@ export default function TimelineF() {
               color={"white"}
               className={styles.shapeLeft}
             >
-              Freelancer
+             NSCC CodeHive
             </Typography>
             <Typography
               color={"white"}
               fontStyle={"italic"}
               className={styles.text}
             >
-              {" "}
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam,
-              optio .{" "}
+              Competitive Programming Contest
             </Typography>
           </TimelineContent>
         </TimelineItem>
@@ -107,22 +105,22 @@ export default function TimelineF() {
             color="grey"
             className="styles.timing"
           >
-            9:30 am
+           <Typography>Thu Jan 28 2023 IST</Typography>
+          <Typography>09:00:00 PM - 12:00:00 AM (3 hrs)</Typography>
           </TimelineOppositeContent>
-
           <TimelineSeparator>
             <TimelineConnector
-              sx={{ background: "#4a404a", width: "40%", height: "2rem" }}
+              sx={{
+                background: "#4a404a",
+                width: "40%",
+                height: "2rem",
+              }}
             />
             <TimelineDot color="secondary" sx={{ margin: 0, padding: "7px" }}>
               {/* <FastfoodIcon /> */}
             </TimelineDot>
             <TimelineConnector
-              sx={{
-                background: "linear-gradient(#4a404a,black)",
-                width: "40%",
-                height: "6rem",
-              }}
+              sx={{ background: "#4a404a", width: "40%", height: "6rem" }}
             />
           </TimelineSeparator>
           <TimelineContent sx={{ py: "12px", px: 2 }}>
@@ -132,16 +130,97 @@ export default function TimelineF() {
               color={"white"}
               className={styles.shape}
             >
-              Freelancer
+              Newton School CodeRush-X
             </Typography>
             <Typography
               color={"white"}
               fontStyle={"italic"}
               className={styles.text}
             >
-              {" "}
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam,
-              optio .{" "}
+             Competitive Programming Contest
+            </Typography>
+          </TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+          <TimelineOppositeContent
+            sx={{ m: "auto 0" }}
+            variant="body2"
+            color={"grey"}
+            className="styles.timing"
+          >
+          <Typography>Thu Jan 29 2023 IST</Typography>
+          <Typography>11:59:59</Typography>
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector
+              sx={{ background: "#4a404a", width: "40%", height: "2rem" }}
+            />
+            <TimelineDot color="secondary" sx={{ margin: 0, padding: "7px" }}>
+              {/* <FastfoodIcon /> */}
+            </TimelineDot>
+            <TimelineConnector
+              sx={{ background: "#4a404a", width: "40%", height: "6rem" }}
+            />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: "12px", px: 2 }}>
+            <Typography
+              variant="h6"
+              component="span"
+              color={"white"}
+              className={styles.shapeLeft}
+            >
+             NSCC WebXplore
+            </Typography>
+            <Typography
+              color={"white"}
+              fontStyle={"italic"}
+              className={styles.text}
+            >
+              Hackathon Ends
+            </Typography>
+          </TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+          <TimelineOppositeContent
+            sx={{ m: "auto 0" }}
+            align="right"
+            variant="body2"
+            color="grey"
+            className="styles.timing"
+          >
+           <Typography>Thu Jan 28 2023 IST</Typography>
+          <Typography>09:00:00 PM - 12:00:00 AM (3 hrs)</Typography>
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector
+              sx={{
+                background: "#4a404a",
+                width: "40%",
+                height: "2rem",
+              }}
+            />
+            <TimelineDot color="secondary" sx={{ margin: 0, padding: "7px" }}>
+              {/* <FastfoodIcon /> */}
+            </TimelineDot>
+            <TimelineConnector
+              sx={{ background:"linear-gradient(#4a404a,black)", width: "40%", height: "6rem" }}
+            />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: "12px", px: 2 }}>
+            <Typography
+              variant="h6"
+              component="span"
+              color={"white"}
+              className={styles.shape}
+            >
+             Happy Valentines Week ❤️
+            </Typography>
+            <Typography
+              color={"white"}
+              fontStyle={"italic"}
+              className={styles.text}
+            >
+             Single Dal at NSCC will host next event in 2nd week of Feb :)
             </Typography>
           </TimelineContent>
         </TimelineItem>
