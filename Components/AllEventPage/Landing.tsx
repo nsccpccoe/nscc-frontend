4675cd692c504c8c5eb21b4b522db0a027559f0e
Dron <dronr19@gmail.com>
2023-01-15 00:55:04 +0530

    event page

--- Components/AllEventPage/Landing.tsx
+++ Components/AllEventPage/Landing.tsx
@@ -2,13 +2,23 @@ import classes from "./Landing.module.css";
 import { FluidContainer } from "../FluidContainer/FluidContainer";
 import EventBox from "./EventBox";
 
+import OngoingEvents from  "../OngoingEvents/OngoingEvents"
+import UpcomingEvents from  "../UpcomingEvents/UpcomingEvents"
+import PastEvents  from  "../PastEvents/PastEvents"
+
+
+
 function Landing() {
   return (
     <div className={classes.mainContainer}>
       <FluidContainer>
         <div className={classes.container}>
           <EventBox />
+          
         </div>
+        <OngoingEvents />
+          <UpcomingEvents />
+          <PastEvents />
       </FluidContainer>
     </div>
   );
--- pages/test.tsx
+++ pages/test.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import Contest_desc from '../Components/Contest_desc/Contest_desc'
-import Contest_prizes from '../Components/Contest_prizes/Contest_prizes'
+
 import OngoingEvents from '../Components/OngoingEvents/OngoingEvents'
 import PastEvents from '../Components/PastEvents/PastEvents'
 import UpcomingEvents from '../Components/UpcomingEvents/UpcomingEvents'
