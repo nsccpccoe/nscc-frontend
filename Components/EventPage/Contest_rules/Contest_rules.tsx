a4c06ed75c26fce42ad33fc6de5174287de69487
Vyankatesh <vrnyati@gmail.com>
2023-01-16 03:26:19 +0530

    changes in event page

---
+++ Components/EventPage/Contest_rules/Contest_rules.tsx
@@ -0,0 +1,29 @@
+import styles from "./Contest_rules.module.css";
+
+function Contest_rules() {
+  return (
+    <div className={styles.main}>
+      <h1>Rules of participation</h1>
+
+      <ul className={styles.content}>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis in
+          beatae quia voluptatem molestiae itaque assumenda reiciendis id
+          cupiditate quis.
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis in
+          beatae quia voluptatem molestiae itaque assumenda reiciendis id
+          cupiditate quis.
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis in
+          beatae quia voluptatem molestiae itaque assumenda reiciendis id
+          cupiditate quis.
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Contest_rules;
