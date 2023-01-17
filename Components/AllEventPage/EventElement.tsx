d184b925401a23fa7935424bb3c613eb5047292c
Onkar Jadhav <omjadhav2610gmail.com>
2023-01-17 05:51:03 +0530

    Check registration status on events landing page

--- Components/AllEventPage/EventElement.tsx
+++ Components/AllEventPage/EventElement.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import classes from "./EventElement.module.css";
 import { Event } from "../interfaces/event.interface";
+import { useEffect, useState } from "react";
 
 type ElementProps = {
   event: Event
@@ -12,12 +13,51 @@ type ElementProps = {
 
 function EventElement(props: ElementProps) {
 
+  const [isRegistered, setIsRegistered] = useState(false);
+  const [error, setError] = useState("");
+  const [token, settoken] = useState("");
+
+  useEffect(() => {
+    // Perform localStorage action
+    settoken(localStorage.getItem("accessToken") || "");
+  }, []);
+
+
+  // fetch registration status
+  useEffect(() => {
+    if (props.event.registration.type != "onsite")
+      return;
+
+    fetch(`https://asia-south1-nsccpccoe.cloudfunctions.net/register/${props.event.id}/status`,
+      { headers: { authorization: `Bearer ${token}` }, }
+    )
+      .then(res => {
+        if (!res.ok) {
+          throw res.status + res.statusText;
+        }
+        return res;
+      })
+      .then(res => res.json())
+      .then(res => setIsRegistered(res?.data?.registered))
+      .catch(e => { setError(e) })
+  }, [])
+
+  let buttonText = "";
+
+  if (error) {
+    buttonText = error.toString();
+  } else {
+    buttonText = isRegistered ? "Registered" : "Register";
+  }
+
+  buttonText = isRegistered ? "Registered" : "Register";
+
   return (
     <Link
       className={classes.container}
       href={props.event.eventPage.link}
       target={props.event.eventPage.type === 'offsite' ? '_blank' : '_self'}
-      >
+    >
       <div className={classes.type}>
         <p>{props.type}</p>
       </div>
@@ -31,7 +71,9 @@ function EventElement(props: ElementProps) {
         className={classes.regButton}
         href={props.event.registration.link}
         target={props.event.registration.type === 'offsite' ? '_blank' : '_self'}
-        >Register</Link>
+      >
+        {buttonText}
+      </Link>
     </Link>
   );
 }
