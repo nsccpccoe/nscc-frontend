86daa9e0e8325b2e0fa6e43a4a528ba140a40ca8
jhasuraj01 <44930179+jhasuraj01@users.noreply.github.com>
2023-01-17 19:08:36 +0000

    added colorfull linl

--- Components/EventPage/ContactDetails.tsx
+++ Components/EventPage/ContactDetails.tsx
@@ -7,11 +7,9 @@ function ContactDetails() {
 
       <ul className={styles.content}>
         <li>
-          <a href="https://chat.whatsapp.com/IzeR7xGsf5270zOvzuHQvD">
-            Click Here for Whatsapp group
-          </a>
+          WhatsApp Group: <a style={{ color: '#7a9ce0', textDecoration: 'none' }} href="https://chat.whatsapp.com/IzeR7xGsf5270zOvzuHQvD">https://chat.whatsapp.com/IzeR7xGsf5270zOvzuHQvD</a>
         </li>
-        <li>Email : nscc@pccoepune.org</li>
+        <li>Email: <a style={{ color: '#7a9ce0', textDecoration: 'none' }} href="mailto:nscc@pccoepune.org">nscc@pccoepune.org</a></li>
       </ul>
     </div>
   );
