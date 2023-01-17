83ff2ff45ab77f8863fb4f564c3ee3df094842fd
Dron <dronr19@gmail.com>
2023-01-17 22:44:57 +0530

    faviconicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,15 @@ import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import 'react-toastify/dist/ReactToastify.min.css'
 import Navbar from '../Components/Header/Header'
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+    
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
        <Navbar /> 
       <Component {...pageProps} />
     </>
